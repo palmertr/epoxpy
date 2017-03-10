d9bb4d454eb4903e1d7243a3970e9de8a0a4db1f
Stephen Thomas <stephenthomas@COENs-iMac-2.boisestate.edu>
2017-03-09 17:00:26 -0700

    Adding simulation class wrappers for epoxy simulation into a package called epoxpy

--- test_simulation.py
+++ test_simulation.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
-from epoxy_simulation import EpoxySimulation
-from job import SingleJob
-from temperature_profile_builder import LinearTemperatureProfileBuilder
+import epoxpy.epoxy_simulation as es
+import epoxpy.job as jb
+import epoxpy.temperature_profile_builder as tpb
 
 print('\n# Test1: Running the simulation in a single job')
 # This simulation should run a total of 700 time steps because the default dt of the HOOMD engine is 1e-2
@@ -10,7 +10,7 @@ md_time = 4e4
 mix_kt = 2.0
 time_scale = 1
 temp_scale = 1
-type_A_md_temp_profile = LinearTemperatureProfileBuilder(initial_temperature=mix_kt, initial_time=mix_time)
+type_A_md_temp_profile = tpb.LinearTemperatureProfileBuilder(initial_temperature=mix_kt, initial_time=mix_time)
 type_A_md_temp_profile.add_state_point(60 * time_scale, 4.5 * temp_scale)
 type_A_md_temp_profile.add_state_point(190 * time_scale, 4.5 * temp_scale)
 type_A_md_temp_profile.add_state_point(240 * time_scale, 1.0 * temp_scale)
@@ -19,8 +19,8 @@ fig = type_A_md_temp_profile.get_figure()
 #plt.show()
 fig.savefig("type_A_temp_profile.png")
 
-myEpoxySim = EpoxySimulation('epoxy_test1', mix_time=mix_time, mix_kt=mix_kt, md_time=md_time,
+myEpoxySim = es.EpoxySimulation('epoxy_test1', mix_time=mix_time, mix_kt=mix_kt, md_time=md_time,
                              temp_prof=type_A_md_temp_profile, n_mul=1.0)
 
-mySingleJobForEpoxy = SingleJob(myEpoxySim)
+mySingleJobForEpoxy = jb.SingleJob(myEpoxySim)
 mySingleJobForEpoxy.execute()
