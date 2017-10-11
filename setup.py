76493aa3ed25062d35d1c45adef62bd730d73e09
Stephen Thomas <stephenthomas1@boisestate.edu>
2017-10-11 12:37:42 -0600

    Removing install requirements from setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ from __future__ import print_function
 from setuptools import setup, find_packages
 
 setup(name='epoxpy',
-        version='2.0',
+        version='2.0.1',
         description='Simulation wrappers for epoxy simulations using HOOMD',
         url='https://bitbucket.org/cmelab/epoxpy',
         author='cmelab',
         author_email='stephenthomas1@boisestate.edu',
-        license='MIT',
+        license='GPL-3.0',
         packages=find_packages(),
         package_dir={'epoxpy':'epoxpy'},
-        install_requires=[
-            'numpy',
-            'matplotlib',
-            'mbuild',
-            'pytest'
-        ],
+#        install_requires=[
+#            'numpy',
+#            'matplotlib',
+#            'mbuild',
+#            'pytest'
+#        ],
         zip_safe=False)
